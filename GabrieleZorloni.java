5c8d6ec11f856bd1a59a327c82819a96450226c7
gabryz95 <gabriele.zorloni@usi.ch>
2020-11-07 17:10:25 +0100

    added GabrieleZorloni class implementation

--- GabrieleZorloni.java
+++ GabrieleZorloni.java
@@ -1,2 +1,21 @@
 public class GabrieleZorloni {
+
+    private final String name = "Gabriele";
+    private final String surname = "Zorloni";
+
+    public GabrieleZorloni() {
+    }
+
+    public void print() {
+        System.out.println("name='" + name + '\'' + ", surname='" + surname + '\'');
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
 }
