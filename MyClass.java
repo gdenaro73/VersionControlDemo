569b6f86eda281b90857ed6919f545b7d6d74210
Aron Dalle Pezze <aron.dalle.pezze@usi.ch>
2021-11-09 10:44:51 +0100

    modified value of nice

--- MyClass.java
+++ MyClass.java
@@ -1,8 +1,8 @@
 public class MyClass {
     
     public String niceMethod() {
-        String Travaglia = "giovanni & ali";        
-        return Travaglia;
+        String DallePezze = "giovanni & ali";        
+        return DallePezze;
     }
     
 }
