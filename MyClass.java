5d7dc1961cfd4fd18468ce23574ae25a8b69bb7e
Giovanni Denaro <denaro@disco.unimib.it>
2021-11-09 14:28:13 +0100

    temporary change to a string

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,7 @@
 public class MyClass {
     
     public String niceMethod() {
-        String nice = "giovanni & ali";        
+        String nice = "we are updating this string at the moment but, if you experience any conflict, please do not hesitate to modify it to set the value that you have chosen";        
         return nice;
     }
     
