66c4fb4b3fe23fd28982a8f6e41c88ea2ea71129
Annonymus <13857960+Annonymus-v02@users.noreply.github.com>
2021-11-10 20:04:20 +0100

    manually clone changes from upstream because github won't let me resolve conflicts from browser
    and I'm too lazy to clone and resolve on my own machine

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,7 @@
 public class MyClass {
 
     public String niceMethod() {
-        String Joe = "joe";
+        String Joe = "we are updating this string at the moment but, if you experience any conflict, please do not hesitate to modify it to set the value that you have chosen";
         return Joe;
     }
 
