f80e3c284596eb7ba3f12e3bdeef65e320940be8
Isabella L Fairman <isabellalfairman@Isabellas-MacBook-Pro-USI.local>
2021-11-11 09:38:20 +0100

    added multiply class

--- IsabellaFairman.java
+++ IsabellaFairman.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class IsabellaFairman {
+public class IsabellaFairman {
+    private String nameSurname;
+
+    public IsabellaFairman(String nameSurname) {
+        this.nameSurname = nameSurname;
+    }
+    public int multiply(int a, int b){return a*b;}
 }
