ccb41d1db19e781ceb3fee445908b95cd9769c1c
Isabella L Fairman <isabellalfairman@Isabellas-MacBook-Pro-USI.local>
2021-11-11 09:44:06 +0100

    changed niceMethod

--- MyClass.java
+++ MyClass.java
@@ -1,8 +1,8 @@
 public class MyClass {
     
     public String niceMethod() {
-        String Travaglia = "giovanni & ali";        
-        return Travaglia;
+        String Fairman= "giovanni & ali";
+        return Fairman;
     }
     
 }
