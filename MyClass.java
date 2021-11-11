33e5c7855c082edf29b1a8e76baacd0c1be59121
simgerdogan <simgepinarerdogan@gmail.com>
2021-11-12 00:01:43 +0100

    modify the variable

--- MyClass.java
+++ MyClass.java
@@ -2,9 +2,9 @@ public class MyClass {
     
     public String niceMethod() {
 
-        String Ener = "we are updating this string at the moment but, if you experience any conflict, please do not hesitate to modify it to set the value that you have chosen";     
-	
-        return Ener;
+        String Erdogan = "we are updating this string at the moment but, if you experience any conflict, please do not hesitate to modify it to set the value that you have chosen";
 
-    
+        return Erdogan;
+
+    }
 }
