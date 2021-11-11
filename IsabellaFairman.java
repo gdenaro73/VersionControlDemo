bd5c60ee114563ba82172474ade9b27dd44b4bf0
Isabella L Fairman <isabellalfairman@Isabellas-MacBook-Pro-USI.local>
2021-11-11 09:43:17 +0100

    added divide method

--- IsabellaFairman.java
+++ IsabellaFairman.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class IsabellaFairman {
+public class IsabellaFairman {
+
+    public IsabellaFairman() {
+        System.out.println("Isabella Fairman");
+    }
+    public int divide(int a, int b){return a/b;}
 }
