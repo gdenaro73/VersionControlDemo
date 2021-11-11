dc97f15abaa70906227222b7e2920a54c57e538f
Ali Egemen Ener <eneregemen@gmail.com>
2021-11-11 18:21:17 +0100

    Chaged variable name

--- MyClass.java
+++ MyClass.java
@@ -1,8 +1,8 @@
 public class MyClass {
     
     public String niceMethod() {
-        String Franchini = "we are updating this string at the moment but, if you experience any conflict, please do not hesitate to modify it to set the value that you have chosen";        
-        return Franchini;
+        String Ener = "we are updating this string at the moment but, if you experience any conflict, please do not hesitate to modify it to set the value that you have chosen";        
+        return Ener;
     }
     
 }
